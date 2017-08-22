5bd64d14847eb3f11d2398e81df476a3e1920c58
Julien Niord <niord.julien@gmail.com>
2017-08-22 17:35:05 +0200

    dcxsq

--- eclipse-workspace/Poro_Link_v2/src/main/java/poroLink/utils/asciiart/AsciiArtMode.java
+++
@@ -1,8 +0,0 @@
-package poroLink.utils.asciiart;
-
-public enum AsciiArtMode {
-	light,
-	huge,
-	random,
-	full,
-}
--- eclipse-workspace/Poro_Link_v2/src/main/java/poroLink/views/IBaseView.java
+++
@@ -1,7 +0,0 @@
-package poroLink.views;
-
-import javax.swing.JFrame;
-
-public interface IBaseView {
-	public void loadView(JFrame frame);
-}
